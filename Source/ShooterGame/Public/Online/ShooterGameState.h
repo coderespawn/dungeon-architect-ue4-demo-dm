a0e38d9419332fc58028576d85850d297ce84d9f
Ali Akbar <ali.akbar@gmail.com>
2016-09-16 13:33:15 +0530

    Initial commit

---
+++ Source/ShooterGame/Public/Online/ShooterGameState.h
@@ -0,0 +1,44 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ShooterGameState.generated.h"
+
+/** ranked PlayerState map, created from the GameState */
+typedef TMap<int32, TWeakObjectPtr<AShooterPlayerState> > RankedPlayerMap; 
+
+UCLASS()
+class AShooterGameState : public AGameState
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	UPROPERTY(Transient, Replicated, BlueprintReadWrite)
+	int32 DungeonSeed;
+
+	/** number of teams in current game (doesn't deprecate when no players are left in a team) */
+	UPROPERTY(Transient, Replicated)
+	int32 NumTeams;
+
+	/** accumulated score per team */
+	UPROPERTY(Transient, Replicated)
+	TArray<int32> TeamScores;
+
+	/** time left for warmup / match */
+	UPROPERTY(Transient, Replicated)
+	int32 RemainingTime;
+
+	/** is timer paused? */
+	UPROPERTY(Transient, Replicated)
+	bool bTimerPaused;
+
+	/** is timer paused? */
+	UPROPERTY(Transient, BlueprintReadWrite)
+	bool bDungeonCreated;
+
+	/** gets ranked PlayerState map for specific team */
+	void GetRankedMap(int32 TeamIndex, RankedPlayerMap& OutRankedMap) const;	
+
+	void RequestFinishAndExitToMainMenu();
+};
---
+++ Source/ShooterGame/Public/Player/ShooterPlayerStart.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ShooterPlayerStart.generated.h"
+
+class UArrowComponent;
+
+UCLASS(Blueprintable, BlueprintType)
+class AShooterPlayerStart : public APlayerStart
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+};

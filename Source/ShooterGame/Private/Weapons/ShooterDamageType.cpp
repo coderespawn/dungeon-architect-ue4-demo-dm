1b2c56bd2bf34d1f28fb6267eff9b5f423898dab
Ali Akbar <ali.akbar@coderespawn.com>
2017-05-10 19:56:41 +0530

    Created a pure BP implementation without source code change

--- Source/ShooterGame/Private/Weapons/ShooterDamageType.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterGame.h"
-#include "Weapons/ShooterDamageType.h"
-
-UShooterDamageType::UShooterDamageType(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
